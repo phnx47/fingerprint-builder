f63c0d56fb3e563454145b27bd0543ac02fea7a1
Sergey Kuznetsov <cmt@phnx47.net>
2019-11-30 17:11:51 +0300

    Add SHA256 tests

--- tests/FingerprintBuilderTests.cs
+++ tests/FingerprintBuilderTests.cs
@@ -1,10 +1,9 @@
-using System;
 using System.Security.Cryptography;
 using Xunit;
 
 namespace FingerprintBuilder.Tests
 {
-    public class UnitTest1
+    public class FingerprintBuilderTests
     {
         [Fact]
         public void UserInfo_Sha1()
@@ -21,7 +20,7 @@ namespace FingerprintBuilder.Tests
 
             Assert.Equal("bfe2cb034d9448e66f642506e6370dd87bbbe0e0", hash);
         }
-        
+
         [Fact]
         public void UserInfo_IgnoreCase_Sha1()
         {
@@ -39,15 +38,47 @@ namespace FingerprintBuilder.Tests
 
             user.FirstName = user.FirstName.ToLowerInvariant();
             user.LastName = user.LastName.ToLowerInvariant();
-            
+
             var hash1 = fingerprint(user).ToHexString();
             Assert.Equal("f747d848c4e4c6fab369e6a4d72e42d764036e98", hash1);
         }
-        
 
         [Fact]
         public void UserInfo_Sha256()
         {
+            var fingerprint = FingerprintBuilder<UserInfo>
+                .Create(SHA256.Create().ComputeHash)
+                .For(p => p.FirstName)
+                .For(p => p.LastName)
+                .Build();
+
+            var user = new UserInfo { FirstName = "John", LastName = "Smith" };
+
+            var hash = fingerprint(user).ToHexString();
+
+            Assert.Equal("9996c4bbc1da4938144886b27b7c680e75932b5a56d911754d75ae4e0a9b4f1a", hash);
+        }
+
+        [Fact]
+        public void UserInfo_IgnoreCase_Sha256()
+        {
+            var fingerprint = FingerprintBuilder<UserInfo>
+                .Create(SHA256.Create().ComputeHash)
+                .For(p => p.FirstName, true, true)
+                .For(p => p.LastName, true, true)
+                .Build();
+
+            var user = new UserInfo { FirstName = "John", LastName = "Smith" };
+
+            var hash = fingerprint(user).ToHexString();
+
+            Assert.Equal("74f20d0059ce864f9e8a4ad337a94dbfed3b00be9b04bbc01d12feec63eb645f", hash);
+
+            user.FirstName = user.FirstName.ToLowerInvariant();
+            user.LastName = user.LastName.ToLowerInvariant();
+
+            var hash1 = fingerprint(user).ToHexString();
+            Assert.Equal("74f20d0059ce864f9e8a4ad337a94dbfed3b00be9b04bbc01d12feec63eb645f", hash1);
         }
 
         private class UserInfo
