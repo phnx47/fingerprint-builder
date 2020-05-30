685beecdb179162f4e28d3fbbfb6615a20ababe9
Sergey Kuznetsov <cmt@phnx47.net>
2020-05-31 00:03:51 +0700

    feat: Benchmarks

---
+++ tests/benchmarks/ModelToHex.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+
+namespace FingerprintBuilder.BenchmarkTests
+{
+    [MeanColumn, MinColumn, MaxColumn, MedianColumn]
+    [SimpleJob(RunStrategy.Throughput, RuntimeMoniker.NetCoreApp31, 2, 10, 50)]
+    public class ModelToHex
+    {
+        private Func<UserInfo, byte[]> _md5;
+        private Func<UserInfo, byte[]> _sha1;
+        private Func<UserInfo, byte[]> _sha256;
+        private Func<UserInfo, byte[]> _sha512;
+
+        private UserInfo _user;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _user = new UserInfo
+            {
+                FirstName = "John",
+                LastName = "Smith"
+            };
+            
+            _md5 = FingerprintBuilder<UserInfo>
+                .Create(MD5.Create().ComputeHash)
+                .For(p => p.FirstName)
+                .For(p => p.LastName)
+                .Build();
+            
+            _sha1 = FingerprintBuilder<UserInfo>
+                .Create(SHA1.Create().ComputeHash)
+                .For(p => p.FirstName)
+                .For(p => p.LastName)
+                .Build();
+            
+            _sha256 = FingerprintBuilder<UserInfo>
+                .Create(SHA256.Create().ComputeHash)
+                .For(p => p.FirstName)
+                .For(p => p.LastName)
+                .Build();
+            
+            _sha512 = FingerprintBuilder<UserInfo>
+                .Create(SHA512.Create().ComputeHash)
+                .For(p => p.FirstName)
+                .For(p => p.LastName)
+                .Build();
+        }
+
+        [Benchmark]
+        public string MD5_Model_To_Hex()
+        {
+            return _md5(_user).ToLowerHexString();
+        }
+        
+        [Benchmark]
+        public string SHA1_Model_To_Hex()
+        {
+            return _sha1(_user).ToLowerHexString();
+        }
+        
+        [Benchmark]
+        public string SHA256_Model_To_Hex()
+        {
+            return _sha256(_user).ToLowerHexString();
+        }
+        
+        [Benchmark]
+        public string SHA512_Model_To_Hex()
+        {
+            return _sha512(_user).ToLowerHexString();
+        }
+
+        private class UserInfo
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+        }
+    }
+}
