958697df4c7807539b44b8d11a4a8221e98fb8aa
Serge K <cmt@phnx47.net>
2023-01-20 17:04:49 +0700

    test: cleanup array tests

--- tests/units/ArrayTests.cs
+++ tests/units/ArrayTests.cs
@@ -7,6 +7,19 @@ namespace FingerprintBuilder.Tests
 {
     public class ArrayTests
     {
+        private readonly Func<ThisUser, byte[]> _fingerprint;
+        private readonly ThisUser _user;
+
+        public ArrayTests()
+        {
+            _fingerprint = FingerprintBuilder<ThisUser>
+                .Create(SHA1.Create())
+                .For(p => p.FirstName)
+                .For(p => p.Emails, emails => string.Join('|', emails))
+                .Build();
+            _user = new ThisUser { FirstName = "John", Emails = new[] { "test@test.com" } };
+        }
+
         [Fact]
         public void EmailsAsArray_ThrowArgumentException()
         {
@@ -21,33 +34,16 @@ namespace FingerprintBuilder.Tests
         [Fact]
         public void Sha1_EmailsToString()
         {
-            var fingerprint = FingerprintBuilder<ThisUser>
-                .Create(SHA1.Create())
-                .For(p => p.FirstName)
-                .For(p => p.Emails, emails => string.Join('|', emails))
-                .Build();
-
-            var user = new ThisUser { FirstName = "John", Emails = new[] { "test@test.com" } };
-
-            var hash = fingerprint(user).ToLowerHexString();
-
+            var hash = _fingerprint(_user).ToLowerHexString();
             Assert.Equal("a9840b53e6aadb3a3a5a48d3ffc9df56441304b6", hash);
         }
 
         [Fact]
         public void Sha1_EmailsToString_UpdateArray_ChangeHash()
         {
-            var fingerprint = FingerprintBuilder<ThisUser>
-                .Create(SHA1.Create())
-                .For(p => p.FirstName)
-                .For(p => p.Emails, emails => string.Join('|', emails))
-                .Build();
-
-            var user = new ThisUser { FirstName = "John", Emails = new[] { "test@test.com" } };
-
-            var hash0 = fingerprint(user).ToLowerHexString();
-            user.Emails[0] += "1";
-            var hash1 = fingerprint(user).ToLowerHexString();
+            var hash0 = _fingerprint(_user).ToLowerHexString();
+            _user.Emails[0] += "1";
+            var hash1 = _fingerprint(_user).ToLowerHexString();
 
             Assert.NotEqual(hash0, hash1);
         }
